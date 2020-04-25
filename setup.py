349c7fad065db7c8fb2af0c5d287ec646b8254ce
devych <devych.code@gmail.com>
2020-04-25 21:15:12 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'tmon_switch_alarm',
+    version          = '1.0',
+    author           = 'devych',
+    author_email     = 'devych.code@gmail.com',
+    install_requires=[
+        'requests',
+        'selenium',
+    ],
+)
