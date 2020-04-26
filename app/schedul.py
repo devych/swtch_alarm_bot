f668a3a3c8e585478651f9be4c619bbc0a2911a1
devych <devych.code@gmail.com>
2020-04-26 15:14:10 +0900

    schedule.py 추가 3분 - + 120초 간격으로 스크랩 요청

---
+++ app/schedul.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+from app.crawler import crawler
+
+schedule = BlockingScheduler()
+
+
+schedule.add_job(crawler, 'interval', id='ntd_swtch_crw', minutes=3, jitter=120)
+
+
+schedule.start()
--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
     install_requires=[
         'requests',
         'selenium',
+        'telegram',
+        'apscheduler',
     ],
 )
