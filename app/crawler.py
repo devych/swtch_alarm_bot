3d3307aa2e4b855bff41b3eab858929bd4486ec9
devych <devych.code@gmail.com>
2020-04-27 14:38:17 +0900

    ssg 사이트와 coupang 사이트 리스트 추가

--- app/crawler.py
+++ app/crawler.py
@@ -1,16 +1,20 @@
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from app.telegram_bot import sendChannelMsg
+from env.path import mac_chromedriver_path, mac_user_chrome_data_path
 
 tmon = "http://www.tmon.co.kr/deal/810158162"
 dwshop = "https://www.daewonshop.com/goods/goods_view.php?goodsNo=1000094791"
+ssg = "http://shinsegaemall.ssg.com/item/itemView.ssg?itemId=1000035429874"
+coupang = "https://www.coupang.com/vp/products/1384804427?isAddedCart="
 
-siteList = [tmon, dwshop]
+siteList = [tmon, dwshop, ssg, coupang]
 
 
 def stock_crawler(url):
-    driver_path = "../chromedriver/chromedriver"
+    driver_path = mac_chromedriver_path
 
     options = Options()
     options.page_load_strategy = 'normal'
@@ -23,7 +27,7 @@ def stock_crawler(url):
 
     options.add_argument('--log-level=3')
     options.add_argument('--disable-loggin')
-    options.add_argument('--user-data-dir=/Users/devych/Library/Application Support/Google/Chrome')
+    options.add_argument(mac_user_chrome_data_path)
     driver = webdriver.Chrome(executable_path=driver_path, options=options)
 
     try:
@@ -63,8 +67,29 @@ def stock_crawler(url):
             else:
                 print('대원샵 재고 있음')
                 sendChannelMsg(f'지금 닌텐도 스위치 동물의 숲 에디션이 입고되었습니다.\n아래 링크로 접속하여 구매하세요.\n{url}')
-    except ValueError:
-        sendChannelMsg(f'에러가 발생했습니다.\n에러 메세지를 확인해 주세요\n{ValueError}')
+
+        elif url is ssg:
+            el_stock = driver.find_elements_by_css_selector('#oriCart')
+            el_stock = el_stock[0].get_attribute('outerHTML')
+            if '품절' in el_stock and 'soldout' in el_stock:
+                # sendChannelMsg(f'현재 ssg에 닌텐도 스위치 동물의 숲 에디션 재고가 없습니다.\n다시 확인하고 알려드릴게요!\n{url}')
+                pass
+            else:
+                print('ssg 재고 있음')
+                sendChannelMsg(f'지금 닌텐도 스위치 동물의 숲 에디션이 입고되었습니다.\n아래 링크로 접속하여 구매하세요.\n{url}')
+        elif url is coupang:
+            el_stock = driver.find_elements_by_css_selector("#contents > div.prod-atf > div > div.prod-buy.sold-out.new-oos-style.not-loyalty-member.eligible-address.without-subscribe-buy-type.DISPLAY_0.only-one-delivery > div.prod-price-container")
+            el_stock = el_stock[0].get_attribute('outerHTML')
+            if '일시품절' in el_stock:
+                # sendChannelMsg(f'현재 coupang에 닌텐도 스위치 동물의 숲 에디션 재고가 없습니다.\n다시 확인하고 알려드릴게요!\n{url}')
+                pass
+            else:
+                print('coupang 재고 있음')
+                sendChannelMsg(f'지금 닌텐도 스위치 동물의 숲 에디션이 입고되었습니다.\n아래 링크로 접속하여 구매하세요.\n{url}')
+
+    except:
+        print('에러 발생')
+        sendChannelMsg(f'에러가 발생했습니다.\n하지만 무언가 바뀌었다는건 재고가 들어왔다는 의미일수도 있어요.\n아래 url로 접속해보세요!\n{url}')
 
     finally:
         driver.quit()
@@ -77,9 +102,7 @@ def crawler():
         stock_crawler(site)
 
 
-
 def show_time():
     get_time = time.ctime()
     print(f'{get_time[8:10]}일 {get_time[11:19]} 현재 크롤링 진행중')
-
 
